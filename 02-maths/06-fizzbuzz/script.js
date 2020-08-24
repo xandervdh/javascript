70ff7c85c827f7683d85329203c71620903325c5
xandervdh <xander-v.d.h@hotmail.com>
2020-08-24 15:15:10 +0200

    finished

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -10,7 +10,16 @@
 // You will have time to focus on it later.
 
 (function() {
-
-    // your code here
+    for (var i = 1; i <= 100; i++){
+        if (i%3 === 0 && i%5 === 0){
+            console.log("fizzbuzz");
+        } else if (i%5 === 0){
+            console.log("buzz");
+        }else if (i%3 === 0){
+            console.log("fizz");
+        }else {
+            console.log(i);
+        }
+    }
 
 })();
