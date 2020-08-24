b1bcccc6c0f215450fad7c383198c8fac486667c
xandervdh <xander-v.d.h@hotmail.com>
2020-08-24 09:19:27 +0200

    finished excersize 2

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -10,7 +10,7 @@
 // You will have time to focus on it later.
 
 (function() {
-
-    // your code here
+    var name = window.prompt("Enter your name: ");
+    alert("Hello " + name + "!");
 
 })();
