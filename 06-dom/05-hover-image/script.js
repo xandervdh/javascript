b13f3fefa5b546863f315edc7dad749fcd62c782
xandervdh <xander-v.d.h@hotmail.com>
2020-08-28 12:08:14 +0200

    finished

---
+++ 06-dom/05-hover-image/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /06-dom/05-hover-image/script.js - 6.5: survol d'image
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    let image = document.getElementsByTagName("img");
+    let source = image[0].dataset.hover;
+    let orSource = image[0].src;
+
+
+    image[0].addEventListener("mouseover", function (){
+        image[0].setAttribute("src", source)
+    });
+
+    image[0].addEventListener("mouseout", function (){
+        image[0].setAttribute("src", orSource);
+    });
+
+})();
