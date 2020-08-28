d504d03e847da6d2fb97f9d0ed0dfe39015093dc
xandervdh <xander-v.d.h@hotmail.com>
2020-08-28 09:40:49 +0200

    finished

---
+++ 06-dom/03-select-three/script.js
@@ -0,0 +1,20 @@
+/* becode/javascript
+ *
+ * /06-dom/03-select-three/script.js - 6.3: sélection multiple par sélecteur css
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    let elements = document.getElementsByClassName("target");
+    for (var x = 0; x <= 4; x++){
+        elements[x].innerHTML = "owned";
+    }
+
+
+})();
