8e62acacbecff3f5f6ea8dbf25461c504a5edff2
xandervdh <xander-v.d.h@hotmail.com>
2020-08-28 12:26:18 +0200

    finished

---
+++ 06-dom/06-mini-carousel/script.js
@@ -0,0 +1,36 @@
+/* becode/javascript
+ *
+ * /06-dom/06-mini-carousel/script.js - 6.6: mini carousel
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    var gallery= [
+        "../../_shared/img/bell.svg",
+        "../../_shared/img/clock.svg",
+        "../../_shared/img/compass.svg",
+        "../../_shared/img/lemon.svg",
+        "../../_shared/img/map.svg",
+    ];
+
+    let image = document.getElementsByTagName("img");
+    let i = 0;
+
+    function nextItem() {
+        i = i + 1;
+        i = i % gallery.length;
+        return gallery[i];
+    }
+
+    document.getElementById("next").addEventListener("click", function (){
+
+        image[0].setAttribute("src", nextItem());
+    });
+
+})();
