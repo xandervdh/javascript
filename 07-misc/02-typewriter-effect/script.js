dd5da17a0b27689675611b174148c518e7525365
xandervdh <xander-v.d.h@hotmail.com>
2020-09-01 15:45:13 +0200

    finished

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -11,6 +11,18 @@
 
 (function() {
 
-    // your code here
+    let words = "Je suis un texte qui va apparaître dans un effet \"machine à écrire\", un peu comme dans les films noirs. Sauf que... je ne sais pas qui est l'assassin ! Y en a-t-il vraiment un ? Habite-t-il au 21 ? Que de mystères !";
+    let characters = words.split('');
+    let target = document.getElementById("target");
+    target.innerHTML = "";
+
+    function showLetters (index){
+        if (index < characters.length){
+            target.append(characters[index++]);
+            setTimeout(function () {showLetters(index);}, 200);
+
+        }
+    }
+    showLetters(0);
 
 })();
