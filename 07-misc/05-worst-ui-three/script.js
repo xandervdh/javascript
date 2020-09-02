5ff71831dd66f6f2f0db8bfedbe1017eb765ae7f
xandervdh <xander-v.d.h@hotmail.com>
2020-09-02 14:02:55 +0200

    changed the number in target the actual number

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -14,6 +14,7 @@
     let buttons = document.getElementsByTagName("button");
     let input = document.getElementsByTagName("input");
     let target = document.getElementById("target");
+    let value = [];
 
     for (let i = 0; i < buttons.length; i++){
 
@@ -21,9 +22,28 @@
         let max = parseInt(input[i].dataset.max);
 
         buttons[i].addEventListener("click", function (){
+            numberCheck();
             let output = Math.floor(Math.random() * (max - min + 1) + min);
+
+            if (output < 10){
+                output = "0" + output;
+            }
+
             input[i].setAttribute("value", output);
+            value[i] = output;
+
+            let number = "0";
+            for (let y = 0; y < value.length; y++){
+                number += value[y];
+            }
+
+            target.innerHTML = number;
         });
+    }
+    function numberCheck() {
+        for (let x = 0; x < input.length; x++){
+            value[x] = input[x].value;
+        }
     }
 
 })();
