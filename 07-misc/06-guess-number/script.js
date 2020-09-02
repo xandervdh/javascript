a33d50712bdf1e91a73526e24271125e4b79ca5d
xandervdh <xander-v.d.h@hotmail.com>
2020-09-02 14:56:45 +0200

    finished

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -9,8 +9,38 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    let randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+    console.log(randomNumber);
+    let guesses = 0;
+    let guess;
+
+
+    function guessing() {
+        guess = window.prompt("what is your guess?");
+    }
+
+    while (guesses < 5) {
+        guessing()
+        if (guesses < 4) {
+            if (guess < randomNumber) {
+                guesses++;
+                alert("the number is higher, gues " + guesses + "/5.");
+                //guessing();
+            } else if (guess > randomNumber) {
+                guesses++;
+                alert("the number is lower, gues " + guesses + "/5.");
+                //guessing();
+            } else {
+                alert("you are right! you got it in " + guesses + " tries.");
+                break;
+            }
+        } else {
+            alert("you lost! the number was " + randomNumber + "! \n better luck next time.");
+            break;
+        }
+    }
+
 
 })();
