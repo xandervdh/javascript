59b605fa68671d8bda5af6d8b7acc735cb3186f1
xandervdh <xander-v.d.h@hotmail.com>
2020-09-03 12:02:16 +0200

    used a single array instead of a nested array

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -15,14 +15,7 @@
     let characters = words.split('');
     let wave = "";
     let size = [];
-    size[0] = [1, 2, 3, 4, 5, 4, 3, 2];
-    size[1] = [2, 3, 4, 5, 4, 3, 2, 1];
-    size[2] = [3, 4, 5, 4, 3, 2, 1, 2];
-    size[3] = [4, 5, 4, 3, 2, 1, 2, 3];
-    size[4] = [5, 4, 3, 2, 1, 2, 3, 4];
-    size[5] = [4, 3, 2, 1, 2, 3, 4, 5];
-    size[6] = [3, 2, 1, 2, 3, 4, 5, 4];
-    size[6] = [2, 1, 2, 3, 4, 5, 4, 3];
+    size = [1, 2, 3, 4, 5, 4, 3, 2];
     for (let i = 0; i < characters.length; i++) {
         wave += "<span>" + characters[i] + "</span>";
     }
@@ -31,20 +24,18 @@
     //span[0].remove();
     let letters = document.getElementsByTagName("span");
     for (let h = 0; h < letters.length; h++) {
-        letters[h].style.fontSize = size[0][h % size[0].length] + "0px";
+        letters[h].style.fontSize = size[h % size.length] + "0px";
     }
-    let array = 0;
-
+    let offset = 0;
     function animation() {
         let o = 0;
         if (o < 10) {
 
             for (let h = 0; h < letters.length; h++) {
-                let move = array % size.length;
-                letters[h].style.fontSize = size[move][h % size[move].length] + "0px";
+                let pointer = h + offset;
+                letters[h].style.fontSize = size[pointer % size.length] + "0px";
             }
-            array++;
-            o++;
+            offset++;
         }
         animate = setTimeout(function () {
             animation();
@@ -58,14 +49,4 @@
     document.getElementById("stop").addEventListener("click", function () {
         clearTimeout(animate);
     });
-
-    /*let letters = document.getElementsByTagName("span");
-    for (let x = 0; x < letters.length; x++){
-        let y = x+1;
-        let array = size[x];
-        console.log(array);
-        letters[y].style.fontSize = size[0][x % size[0].length] + "0px";
-    }*/
-
-
 })();
