4d5dc744dee2b4d97760af2922ee8d6089856f34
xandervdh <xander-v.d.h@hotmail.com>
2020-09-08 14:46:36 +0200

    9.4 finished after some problems

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -10,5 +10,34 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let submit = document.getElementById("run");
+    let nameInput = document.getElementById("hero-name");
+    let alterEgoInput = document.getElementById("hero-alter-ego");
+    let powersInput = document.getElementById("hero-powers");
+    let name;
+    let alterEgo;
+    let powers;
+
+
+    submit.addEventListener("click", function () {
+        if (nameInput.value !== "" && alterEgoInput.value !== "" && powersInput.value !== "") {
+            name = nameInput.value;
+            alterEgo = alterEgoInput.value;
+            powers = powersInput.value.split(",");
+            fetch("http://localhost:3000/heroes")
+                .then(response => response.json())
+                .then(data => addToData(data))
+        } else {
+            nameInput.style.border = "1px solid red";
+            alterEgoInput.style.border = "1px solid red";
+            powersInput.style.border = "1px solid red";
+        }
+    })
+
+    function addToData(data) {
+
+        data.push({"id": data.length+1, "name": name, "alterEgo": alterEgo, "abilities": powers});
+        console.log(data);
+    }
+
 })();
