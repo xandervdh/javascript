4e8514622c7783f189ed989c334312eba131f0cf
xandervdh <xander-v.d.h@hotmail.com>
2020-09-08 11:28:06 +0200

    9.2 finished

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -10,5 +10,23 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let run = document.getElementById("run");
+    run.addEventListener("click", function (){
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(data => appendData(data))
+        function appendData(data){
+            console.log(data);
+            let target = document.getElementById("target");
+            for (let i = 0; i < data.length; i++){
+                let div = document.createElement("div");
+                div.innerHTML = '<h4 class="title">\n' +
+                    '<strong class="name">' + data[i].name + '</strong>\n' +
+                    '<em class="alter-ego">' + data[i].alterEgo + '</em>\n' +
+                    '</h4>\n' +
+                    '<p class="powers">' + data[i].abilities + '</p>'
+                target.appendChild(div);
+            }
+        }
+    })
 })();
