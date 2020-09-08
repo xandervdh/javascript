94770a05d88911bd8db957917b1b419e3445a28b
xandervdh <xander-v.d.h@hotmail.com>
2020-09-08 10:25:49 +0200

    9.1 finished

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -10,5 +10,10 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let run = document.getElementById("run");
+    run.addEventListener("click", function (){
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    })
 })();
